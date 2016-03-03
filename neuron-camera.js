1cd15b7876005c252065634b9a58449c05464b42
jfellus <jerome.fellus@partnering.fr>
2016-03-03 16:29:48 +0100

    Added stop() notifications

--- neuron-camera.js
+++ neuron-camera.js
@@ -21,7 +21,8 @@ Polymer({
 	is: 'neuron-camera',
 	properties: {
 		width: { notify: true, value:320 },
-		height: { notify: true, value:240 }
+		height: { notify: true, value:240 },
+		camera: { value:null }
 	},
 	ready: function () {
 		var that = this;
@@ -30,13 +31,19 @@ Polymer({
 
 	start: function () {
 		var that = this;
-		navigator.getUserMedia({video:{
-			mandatory:{
-				minWidth:this.width,minHeight:this.height,
-				maxWidth:this.width,maxHeight:this.height
-				//minFrameRate:1,maxFrameRate:1
+		var constraints = {
+			video:{
+				mandatory:{
+					minWidth:this.width,minHeight:this.height,
+					maxWidth:this.width,maxHeight:this.height
+					//minFrameRate:1,maxFrameRate:1
+				}
 			}
-		}}, function (stream) {
+		};
+		if(this.camera==="front") constraints.facingMode = "user";
+		else if(this.camera==="rear") constraints.facingMode = "environment";
+		navigator.getUserMedia(constraints, function (stream) {
+			that.stream = stream;
 			that.fire("stream", {stream:stream}); // Notify the underlying diya-sdk RTC channel that a new stream is available
 			that.video.src = window.URL.createObjectURL(stream);
 			that.video.controls = true;
@@ -45,9 +52,11 @@ Polymer({
 	},
 
 	stop: function () {
-		this.video.pause();
-		if(this.localMediaStream && this.localMediaStream.stop) this.localMediaStream.stop();  // Doesn't do anything in Chrome.
+		if(!this.stream) return;
+		this.stream.getAudioTracks().forEach(function(track) { track.stop();  });
+		this.stream.getVideoTracks().forEach(function(track) { track.stop();  });
 		this.video.src = "";
+		this.fire("stop", {stream:this.stream});
 	}
 });
 
